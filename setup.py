0217f97ffc81f82a73186de41b3edc472d4b3810
A7medHanyFCAI <ahmed.hany.fcai@gmail.com>
2025-11-09 19:10:15 +0200

    Installed Requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ahmed Hany",
+    author_email="ahmed.hany@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
